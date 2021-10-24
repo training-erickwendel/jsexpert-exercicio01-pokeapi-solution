725e402330b12114def2c9bbe3cc465336daaad7
WellsSA <wel.cavzod@gmail.com>
2021-10-24 04:14:07 -0300

    feat(teamRepository): initial repository definition

---
+++ src/repository/teamRepository.js
@@ -0,0 +1,12 @@
+// const https = require('https');
+
+const API_BASE_URL = 'https://pokeapi.co/api/v2';
+
+class TeamRepository {
+  async makeRequest(url) {
+    console.info({ url, API_BASE_URL });
+    return null;
+  }
+}
+
+module.exports = TeamRepository;
